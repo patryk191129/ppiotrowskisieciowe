24bfe862f7d0b51a1683e660a94b6abd0924fd2e
DESKTOP-42OG0EC\Patryk <patryk.piotrowski19@gmail.com>
2018-04-06 14:16:48 +0200

    Wstępnie przygotowano lab2

---
+++ Lab2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            POP3Client _POP3Client = new POP3Client();
+
+
+        }
+    }
+}
